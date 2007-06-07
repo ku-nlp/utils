6652f761871cfe2e037fff4c488056fc8a653433
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2007-06-07 04:19:02 +0000

    主な関数をcolorKNP.pmに移動

--- scripts/color-knp.pl
+++ scripts/color-knp.pl
@@ -1,15 +1,17 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 # $Id$
 
+# usage: echo 'XXX' | juman | knp -tab | perl -I../perl color-knp.pl
+
 use strict;
 use Getopt::Long;
-use Term::ANSIColor;
 use KNP;
+use ColorKNP;
 
-my ($sid, $flag, %feature, %opt);
+my ($sid, $flag, %feature_color, %opt);
 
-GetOptions(\%opt, 'color=s', 'html', 'ansi', 'soft', 'normal', 'hard', 'mrph', 'tag', 'bnst', 'detail', 'line', 'h', 'help');
+GetOptions(\%opt, 'color=s', 'html', 'ansi', 'soft', 'normal', 'hard', 'mrph', 'tag', 'bnst', 'detail', 'line', 'nedefaultcolor', 'h', 'help');
 
 if ($opt{h} || $opt{help}) {
     die "Usage : $0\n" .
@@ -30,35 +32,39 @@ $opt{mrph} = 1 if (!$opt{tag} && !$opt{bnst});
 $opt{normal} = 1 if (!$opt{hard} && !$opt{soft});
 
 # defaultの設定
-# %feature = ("漢字" => "red");
-# if ($opt{ansi}) {
-#     %feature = ("NE:ORGANIZATION" => "blue",
-# 		"NE:PERSON"        => "red",
-# 		"NE:LOCATION"      => "green",
-# 		"NE:ARTIFACT"      => "magenta",
-# 		"NE:DATE"          => "yellow",
-# 		"NE:TIME"          => "cyan",
-# 		"NE:MONEY"         => "olive",
-# 		"NE:PERCENT"       => "maroon");
-# } 
-# else {
-#     %feature = ("NE:ORGANIZATION" => "blue",
-# 		"NE:PERSON"        => "red",
-# 		"NE:LOCATION"      => "green",
-# 		"NE:ARTIFACT"      => "fuchsia",
-# 		"NE:DATE"          => "lime",
-# 		"NE:TIME"          => "aqua",
-# 		"NE:MONEY"         => "olive",
-# 		"NE:PERCENT"       => "maroon");
-# }
+# %feature_color = ("漢字" => "red");
+if ($opt{nedefaultcolor}) {
+    if ($opt{ansi}) {
+	%feature_color = ("NE:ORGANIZATION" => "blue",
+			  "NE:PERSON"        => "red",
+			  "NE:LOCATION"      => "green",
+			  "NE:ARTIFACT"      => "magenta",
+			  "NE:DATE"          => "yellow",
+			  "NE:TIME"          => "cyan",
+			  "NE:MONEY"         => "olive",
+			  "NE:PERCENT"       => "maroon");
+    } 
+    else {
+	%feature_color = ("NE:ORGANIZATION" => "blue",
+			  "NE:PERSON"        => "red",
+			  "NE:LOCATION"      => "green",
+			  "NE:ARTIFACT"      => "fuchsia",
+			  "NE:DATE"          => "lime",
+			  "NE:TIME"          => "aqua",
+			  "NE:MONEY"         => "olive",
+			  "NE:PERCENT"       => "maroon");
+    }
+}
 
 if ($opt{color}) {
     for (split(',', $opt{color})) {
 	if (/(.*)=(.*)/) {
-	    $feature{$1} = $2;
+	    $feature_color{$1} = $2;
 	}
     }
 }
+
+my $colorknp = new ColorKNP(\%feature_color, \%opt);
 
 print "<html><body>\n" if ($opt{html}); 
 
@@ -87,7 +93,7 @@ while (<STDIN>) {
     elsif (/EOS/) {
 	my $result = new KNP::Result($knp_buf);
 
-	&output_result($result);
+	print $colorknp->AddColor($result);
 	$knp_buf = "";
 
 	print "<BR>" if ($opt{html});
@@ -98,74 +104,3 @@ while (<STDIN>) {
 
 print "</body></html><BR>\n" if ($opt{html});
 
-# 出力
-sub output_result {
-    my ($result) = @_;
-
-    my $string;
-    if ($opt{mrph}) {
-	foreach my $mrph ($result->mrph) {
-	    my $string = $mrph->midasi;
-
-	    # 形態素の場合は品詞も対象に
-	    my $feature =  "<" . $mrph->hinsi . ">" . "<" . $mrph->bunrui . ">" . $mrph->fstring;
-	    &add_color($string, $feature);
-	}
-    }
-    elsif ($opt{tag}) {
-	foreach my $tag ($result->tag) {
-	    my $string;
-	    foreach my $mrph ($tag->mrph) {
-		$string .= $mrph->midasi;
-	    }
-	    &add_color($string, $tag->fstring);
-	}
-    }
-    elsif ($opt{bnst}) {
-	foreach my $bnst ($result->bnst) {
-	    my $string;
-	    foreach my $mrph ($bnst->mrph) {
-		$string .= $mrph->midasi;
-	    }
-	    &add_color($string, $bnst->fstring);
-	}
-    }
-}
-
-# 色をつける
-sub add_color {
-    my ($string, $feature) = @_;
-
-    my $color;
-    my $detail;
-
-    for my $f (keys %feature) {
-	if ($opt{normal} && $feature =~ /<($f.*?)>/ ||
-	    $opt{soft} && $feature =~ /<([^>]*$f.*?)>/ ||
-	    $opt{hard} && $feature =~ /<($f)[:>]/) {
-	    $color = $feature{$f};
-	    if ($opt{detail}) {
-		$detail ? $detail .= ",$1" : $detail = $1;
-	    }
-	    last;
-	}
-    }
-
-    if ($opt{html}) {
-	print "<font color = $color>" if ($color && !$detail);
-	print $string;
-	print "<font color = $color>" if ($color && $detail);
-	print '<code class="attn">&lt;</code>' . $detail 
-	    . '<code class="attn">&gt;</code>'if ($opt{detail} && $detail);
-	print '</font>' if ($color);
-    }
-    else {
-	print color($color) if ($color && !$detail);
-	print $string;
-	print color($color) if ($color && $detail);
-	print "<$detail>" if ($opt{detail} && $detail);
-	print color("reset") if ($color);
-    }	
-
-    print '|' if $opt{line};
-}
