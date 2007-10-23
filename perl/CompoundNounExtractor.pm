e45ffdfb026d1d968e4fe7556b4906ed28408832
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2007-10-23 03:08:27 +0000

    複合名詞の先頭/途中/最後にこれる条件を最初にチェックすることにより、何度も条件をチェックせずにすむように変更

--- perl/CompoundNounExtractor.pm
+++ perl/CompoundNounExtractor.pm
@@ -34,7 +34,7 @@ sub new {
 
     $this = {};
 
-    $this->{opt} = $option;
+    $this->{option} = $option;
 
     bless $this;
 
@@ -51,105 +51,97 @@ sub ExtractCompoundNounfromBnst {
     my ($this, $bnst, $option) = @_;
 
     my @word_list;
-    # タグから固有表現を抽出
-    my %ne_list;
-    foreach my $tag ($bnst->tag) {
-	if ($tag->fstring =~ /<NE:([^>]*)>/) {
-	    my ($type, $ne) = split (/:/, $1);
-	    $ne_list{$ne} = $type;
-	}
+
+    # 複合名詞の先頭/真ん中/末尾に来ることができるかをチェック
+    my (@is_ok_for_head, @is_ok_for_mid, @is_ok_for_tail); 
+    my @mrph_list = $bnst->mrph;
+    for my $i (0..$#mrph_list){
+	my $mrph = $mrph_list[$i];
+	my $midasi  = $mrph->midasi;
+	my $fstring = $mrph->fstring;
+	my $bunrui  = $mrph->bunrui;
+	my $hinsi   = $mrph->hinsi;
+
+	# 先頭
+	$is_ok_for_head[$i] = &check_condition_head($midasi, $fstring, $bunrui, $hinsi);
+
+	# 真ん中
+	$is_ok_for_mid[$i] = &check_condition_mid($midasi, $fstring, $bunrui, $hinsi);
+
+	# 末尾
+	$is_ok_for_tail[$i] = &check_condition_tail($midasi, $fstring, $bunrui, $hinsi);
     }
 
-    my $outputted_flag = 0;
+    # debug print
+    if ($this->{option}{debug}){
+	&print_conditions(\@mrph_list, \@is_ok_for_head, \@is_ok_for_mid, \@is_ok_for_tail);
+    }
+
     # ループを回して複合名詞を探す。
     #
     # ex) 自然 言語 処理 と は、
     #          $j  $i
-    my @mrph_list = $bnst->mrph;
-    for (my $i = $#mrph_list; $i >= 0; $i--) {
-	my $mrph = $mrph_list[$i];
 
-	# 複合名詞の最後に来られない形態素
-#	next if ($self->is_stopword ($mrph, 'suffix'));
-
-	# stopword
-#	next if ($self->is_stopword ($mrph));
-
-	# ひらがな一文字
-	next if $mrph->midasi =~ /^\p{Hiragana}$/;
-	
-#	# 固有名詞の途中で終るものは登録しない
-#	next unless ($mrph->fstring =~ /<NE:[A-Z]*:(head|middle)>/);
-
-#	# 一番最後が名詞でなければ次へ
-	# <かな漢字>を条件に入れることにより、「東京都」の「都」が除かれなくなる
-	next unless ($mrph->fstring =~ /<(?:名詞相当語|かな漢字|カタカナ)>/);
-
-	# 形式名詞（もの, こと..)/副詞的名詞(よう, とき..)
-	next if $mrph->bunrui =~ /(?:副詞的|形式)名詞/;
-
-	# 一番最後が記号や数字なら次へ
-	# 名詞相当語 かつ 記号 .. ●,《, ＠など
-	# 名詞相当語 かつ 時間辞 .. 1960年代半ばの「代」など
-	# 数字 .. もし入れると新聞記事やブログの日付が大量に混入?
-	next if ($mrph->fstring =~ /<記号>|<数字>/);
-
-	# 接頭辞で終ってはいけない
-	# 「イースター島再来訪」から「イースター島再」を排除
-	next if ($mrph->fstring =~ /<非?独立タグ接頭辞>/);
-
-	# 非独立接尾辞はNG ただし<意味有>がついている(個、つ、県、化、性など)ならばOK
-	next if $mrph->fstring =~ /<非独立タグ接尾辞>/ && $mrph->fstring !~ /<意味有>/;
+    my $longest_tail_flag = 0;
+    my $outputted_flag = 0;
+    my @mrph_used_num; # 各形態素について、複合名詞の要素となった回数を記録
+    for my $i (reverse(0..$#mrph_list)){
 
 	my $mrph_num = 0;
 	my $midasi = '';
 	my $repname = '';
+	my $jiritsu_mrph_num = 0; #接頭辞、接尾辞を除いた形態素数
+	                          #★「自立」という表現はよろしくない気がする
+
+	my $midasi_i =  $mrph_list[$i]->{midasi};
+	my $fstring_i = $mrph_list[$i]->{fstring};
+
+	if (!$is_ok_for_tail[$i]){
+	    $longest_tail_flag = 0;
+	    next;
+	}
+
+	$longest_tail_flag++;
+
 	#　一番最後を固定してループ
-	for (my $j = $i; $j >= 0; $j--) {
+	for my $j (reverse(0..$i)) {
 
-	    # 名詞か独立タグ接頭辞なら連結
-	    #  <複合←>で捕捉したいもの 「・」「アルファベット一文字」「ドル」「グラム」「ｋｇ」など(たぶん)
-	    #  <複合←>かつ<記号> .. ；＆など多数
-	    # 独立タグ接頭辞 .. 旧、全、不など
-	    # 非独立タグ接頭辞 .. 「お見舞い」の「お」などは入れない
-	    # 問題点：「・」は、複合語の頭や最後にはきてほしくないが、
-	    #         「ドル」は、最後ならばきても良い。
-	    my $mrph2 = $mrph_list[$j];
+	    my $mrph_j = $mrph_list[$j];
+	    my $midasi_j = $mrph_j->midasi;
+	    my $hinsi_j = $mrph_j->hinsi;
 
-#	    # 固有名詞の途中から始まるものは登録しない
-#	    next unless ($mrph->fstring =~ /<NE:[A-Z]*:(middle|tail)>/);
-
-	    last unless ($mrph2->fstring =~ /<名詞相当語>|<漢字>|<独立タグ接頭辞>|<複合←>/);
-
-	    last if $mrph2->fstring =~ /<記号>/ || $mrph2->bunrui =~ /(?:副詞的|形式)名詞/;
+	    if (!$is_ok_for_mid[$j]){
+		last;
+	    }
 
 	    $mrph_num++;
+	    $jiritsu_mrph_num++ if ($hinsi_j ne '接頭辞' && $hinsi_j ne '接尾辞');
 
-	    $midasi = $mrph2->midasi . $midasi;
-	    my $tmp = $mrph2->repname ();
-	    $repname = (($tmp)? $tmp : $mrph2->midasi) . (($repname)?  '+' .  $repname : '');
+	    $midasi = $midasi_j . $midasi;
+	    my $tmp = $mrph_j->repname();
+	    $repname = (($tmp)? $tmp : $midasi_j) . (($repname)?  '+' .  $repname : '');
 
 	    last if ($mrph_num > $MRPH_NUM_MAX);
 
 	    # 一語ばかりからなる複合語は大抵ごみ (文字化けなど)
 	    last if ($mrph_num >= 5 && length ($midasi) <= $mrph_num);
 
-	    # 複合語の途中に来ても良いが、先頭に来るのは変なものを排除する条件群
-	    if ($mrph2->fstring =~ /<名詞相当語>|<漢字>|<独立タグ接頭辞>/
-		&& $mrph2->hinsi ne '接尾辞' # 「-性海棉状脳症」などを除く
-#		&& !$self->is_stopword ($mrph2, 'prefix')
-#		&& $mrph2->fstring !~ /末尾/ && # 人名末尾, 組織名末尾などで終るものを除く
-		&& $mrph2->midasi !~ /^(?:$NG_CHAR)/) {
-#		if ($ne_list{$midasi}) {
-#		    push @word_list, [$midasi, $repname];
-#		} else {
-#		    push @word_list, [$midasi, $repname, $ne_list{$midasi}];
-#		}
-		push @word_list, { midasi => $midasi, repname => $repname, mrphnum => $mrph_num };
+	    next if (!$is_ok_for_head[$j]);
 
-		print "register $midasi\n" if ($this->{option}{debug});
-		$outputted_flag = 1;
+	    $mrph_used_num[$j]++;
+	    push @word_list, { midasi => $midasi, repname => $repname, mrphnum => $mrph_num , jiritsu_mrph_num => $jiritsu_mrph_num};
+
+	    # 末尾の形態素が未定義語ならば、その複合名詞にundef_flagを追加
+	    # ただし、「品曖-その他」または「品曖-カタカナ」のみを未定義語とみなし、
+	    # 「品曖-アルファベット」は、みなさない
+	    if ($fstring_i =~ /品詞変更:[^\d]*\-15\-[1-2]\-/) {
+		if ($word_list[-1]->{mrphnum} == 1) {
+		    $word_list[-1]->{undef_flag} = 1;
+		}
 	    }
+
+	    print "register $midasi\n" if ($this->{option}{debug});
+	    $outputted_flag = 1;
 	}
 
 	# 最長
@@ -158,6 +150,72 @@ sub ExtractCompoundNounfromBnst {
 	}
     }
     return wantarray ? @word_list : $word_list[-1];
+}
+
+# 先頭に来れるかどうかをチェック
+sub check_condition_head {
+    my ($midasi, $fstring, $bunrui, $hinsi) = @_;
+
+    if ($fstring =~ /<名詞相当語>|<漢字>|<独立タグ接頭辞>/
+	&& $hinsi ne '接尾辞' # 「-性海棉状脳症」などを除く
+#		&& !$self->is_stopword ($mrph2, 'prefix')
+#		&& $mrph2->fstring !~ /末尾/ && # 人名末尾, 組織名末尾などで終るものを除く
+	&& $midasi !~ /^(?:$NG_CHAR)/) {
+	return 1;
+    }
+    else {
+	return 0;
+    }
+}
+
+# 真ん中に来れるかどうかをチェック
+sub check_condition_mid {
+    my ($midasi, $fstring, $bunrui, $hinsi) = @_;
+
+    if ($fstring !~ /<(?:名詞相当語|漢字|独立タグ接頭辞|複合←)>/
+	|| $fstring =~ /<記号>/
+	|| $bunrui =~ /(?:副詞的|形式)名詞/) {
+	return 0;
+    }
+    else {
+	return 1;
+    }
+}
+
+# 最後に来れるかどうかをチェック
+sub check_condition_tail {
+    my ($midasi, $fstring, $bunrui, $hinsi) = @_;
+
+    if ($midasi =~ /^\p{Hiragana}$/ # ひらがな一文字
+	# || $mrph->fstring =~ /<NE:[A-Z]*:(head|middle)>/ # 固有名詞の途中で終るものは登録しない
+	|| $fstring !~ /<(?:名詞相当語|かな漢字|カタカナ)>/ # 一番最後が名詞でない
+	|| $bunrui =~ /(?:副詞的|形式)名詞/ # 形式名詞（もの, こと..)/副詞的名詞(よう, とき..)
+	|| $fstring =~ /<記号>|<数字>/ # 名詞相当語 かつ 記号 .. ●,《, ＠など
+                                       # 名詞相当語 かつ 時間辞 .. 1960年代半ばの「代」など
+                                       # 数字 .. もし入れると新聞記事やブログの日付が大量に混入?
+	|| $fstring =~ /<非?独立タグ接頭辞>/ # 「イースター島再来訪」から「イースター島再」を排除
+	|| ($fstring =~ /<非独立タグ接尾辞>/ && $fstring !~ /<意味有>/)) { # 非独立接尾辞はNG ただし<意味有>がついている(個、つ、県、化、性など)ならばOK
+	return 0;
+    }
+    else {
+	return 1;
+    }
+}
+
+sub print_conditions {
+    my ($mrph_list, $is_ok_for_head, $is_ok_for_mid, $is_ok_for_tail) = @_;
+
+    my @mrph_list = @{$mrph_list};
+
+    for my $i (0..$#mrph_list){
+	my $mrph = $mrph_list[$i];
+	print $mrph->midasi . "\n";
+	print "  " . $mrph->hinsi . "\n";
+	print "  " . $mrph->bunrui . "\n";
+	print "  head $is_ok_for_head->[$i]  tail $is_ok_for_tail->[$i]  mid $is_ok_for_mid->[$i]\n";
+	print "  " . $mrph->fstring . "\n";
+	print "\n";
+    }
 }
 
 1;
