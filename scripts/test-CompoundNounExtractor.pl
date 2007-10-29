ef893e3b84f7c98d16fd620a11f284af2ace60a5
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2007-10-29 17:12:48 +0000

    複合名詞中の中黒数の上限数をオプションで指定できるように

--- scripts/test-CompoundNounExtractor.pl
+++ scripts/test-CompoundNounExtractor.pl
@@ -15,7 +15,7 @@ use CompoundNounExtractor;
 use Getopt::Long;
 
 my (%opt);
-GetOptions(\%opt, 'longest', 'clustering', 'mrphnummax=i', 'lengthmax=i', 'length_max_one_word_each=i', 'debug');
+GetOptions(\%opt, 'longest', 'clustering', 'mrphnummax=i', 'lengthmax=i', 'length_max_one_word_each=i', 'centered_dot_num_max=i', 'debug');
 &usage if $opt{help};
 
 my $option;
@@ -24,6 +24,7 @@ $option->{clustering} = 1 if $opt{clustering};
 $option->{MRPH_NUM_MAX} = $opt{mrphnummax} if $opt{mrphnummax};
 $option->{LENGTH_MAX} = $opt{lengthmax} if $opt{lengthmax};
 $option->{LENGTH_MAX_ONE_WORD_EACH} = $opt{length_max_one_word_each} if $opt{length_max_one_word_each};
+$option->{CENTERED_DOT_NUM_MAX} = $opt{centered_dot_num_max} if $opt{centered_dot_num_max};
 my $cne = new CompoundNounExtractor($option);
 
 my $buf;
