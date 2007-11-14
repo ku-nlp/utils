8d70e1707becd8300d2be787b08976c60347046f
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2007-11-14 13:08:41 +0000

    get_verboseオプションを追加

--- scripts/test-CompoundNounExtractor.pl
+++ scripts/test-CompoundNounExtractor.pl
@@ -15,12 +15,13 @@ use CompoundNounExtractor;
 use Getopt::Long;
 
 my (%opt);
-GetOptions(\%opt, 'longest', 'clustering', 'mrphnummax=i', 'lengthmax=i', 'length_max_one_word_each=i', 'centered_dot_num_max=i', 'debug');
+GetOptions(\%opt, 'longest', 'clustering', 'mrphnummax=i', 'lengthmax=i', 'length_max_one_word_each=i', 'centered_dot_num_max=i', 'get_verbose', 'debug');
 &usage if $opt{help};
 
 my $option;
 $option->{debug} = 1 if $opt{debug};
 $option->{clustering} = 1 if $opt{clustering};
+$option->{get_verbose} = 1 if $opt{get_verbose};
 $option->{MRPH_NUM_MAX} = $opt{mrphnummax} if $opt{mrphnummax};
 $option->{LENGTH_MAX} = $opt{lengthmax} if $opt{lengthmax};
 $option->{LENGTH_MAX_ONE_WORD_EACH} = $opt{length_max_one_word_each} if $opt{length_max_one_word_each};
@@ -53,12 +54,14 @@ while (<>) {
 		my @words = $cne->ExtractCompoundNounfromBnst($bnst);
 
 		foreach my $tmp (@words) {
-		    print $tmp->{midasi}, "\n";
+		    print $tmp->{midasi};
+		    print " $tmp->{verbose}" if $opt{get_verbose};
+		    print "\n";
 		}
 	    }
 	}
 
-	$buf = "";
+	$buf = '';
     }
 }
 
