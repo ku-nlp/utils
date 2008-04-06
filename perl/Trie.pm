72ed389f88a420799da7059cf08a3c2745dc2d60
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2008-04-06 04:12:33 +0000

    形態素解析を行うオプションを追加

--- perl/Trie.pm
+++ perl/Trie.pm
@@ -5,13 +5,23 @@ package Trie;
 use utf8;
 use strict;
 use Regexp::Trie;
+use Unicode::Japanese;
+use JICFS;
 
 sub new {
-    my ($this) = @_;
+    my ($this, $opt) = @_;
 
     $this = {
-	trie => Regexp::Trie->new
+	trie => Regexp::Trie->new,
+	jicfs => new JICFS,
+	opt => $opt
     };
+
+    # 形態素解析を行う
+    if ($opt->{usejuman}) {
+	require Juman;
+	$this->{juman} = new Juman;
+    }
 
     bless $this;
 
@@ -26,7 +36,38 @@ sub DESTROY {
 sub Add {
     my ($this, $string) = @_;
 
-    $this->{trie}->add($string);
+    $string =~ s/^\s+//;
+    $string =~ s/\s+$//;
+
+    if ($this->{opt}{usejuman}) {
+	$string = $this->{jicfs}->ArrangeSentence($string);
+	my $ref  = $this->{trie};
+	my $result = $this->{juman}->analysis($string);
+
+	for my $mrph ($result->mrph){
+	    my $repname = $this->GetRepname($mrph);
+
+	    $ref->{$repname} ||= {};
+	    $ref = $ref->{$repname};
+	}
+	$ref->{''} = 1; # { '' => 1 } as terminator
+    }
+    else {
+	$this->{trie}->add($string);
+    }
+}
+
+sub GetRepname {
+    my ($this, $mrph) = @_;
+
+    my $repname = $mrph->repname;
+
+    if ($repname) {
+	return $repname;
+    }
+    else {
+	return $mrph->genkei . '/' . $mrph->yomi;
+    }
 }
 
 # 正規表現をはく
