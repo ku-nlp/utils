402245ef854a86242f3d866dfb8516fde26cfab1
Rintaro Terada <terada@nlp.ist.i.kyoto-u.ac.jp>
2015-06-09 13:46:18 +0900

    出力形式を"親文節の代表表記-子文節の代表表記"のリストに変更

--- python/DependencyParser.py
+++ python/DependencyParser.py
@@ -2,40 +2,36 @@
 
 from pyknp import KNP
 import sys
+import codecs
 
 
+# bnst_list内の係り受け関係のある文節を"親文節-子文節"の形式のリストにして出力
 def parseDependency(bnst_list):
-    parent_and_child = []  # list of [parent,child]
+    parent_child = []
     for bnst in bnst_list:
         if bnst.parent:
-            parent_and_child.append([bnst.parent, bnst])
-    dependency = [] 
-    for pac in parent_and_child:
-        dependent_repname = []
-        for bnst in pac:
-            dependent_repname.append("".join(mrph.repname for mrph in bnst.mrph_list()))
-        dependency.append(dependent_repname)
-    return dependency
+            parent_child.append("-".join([bnst.parent.repname, bnst.repname]))
+    return parent_child
 
 
 def test():
+    # ex.)echo "私は自然言語処理の研究をする"  | juman | knp -tab -dpnd | python DependencyParser.py
+    sys.stdin  = codecs.getreader('UTF-8')(sys.stdin)
+    sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
+    sys.stderr = codecs.getwriter('UTF-8')(sys.stderr)
+    
     knp = KNP()
-
     data = u""
-    i = 0
+
     for line in iter(sys.stdin.readline, ""):
-        i += 1
-        print i
-        data += line.strip().replace(":", ",").decode("utf-8")
-        if data == u"":
-            continue
-        result = knp.parse(data)
-        DB = parseDependency(result.bnst_list())
-        for bnstlist in DB:
-            for repname in bnstlist:
-                print repname.encode("utf-8"),
-            print
-        data = u""
+        data += line
+        if line.strip() == u"EOS":
+            result = knp.result(data)
+            DB = parseDependency(result.bnst_list())
+            print "parent-child"
+            for bnstrep in DB:
+                print bnstrep
+                data = u""
 
 if __name__ == "__main__":
     test()
